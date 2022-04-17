c9d7a4e69df2cecf5afd39cf8656f20ac168ae94
AlanJLJ2202 <lrs19110248@purisima.tecnm.mx>
2022-04-17 12:52:30 -0500

    Second

--- index.php
+++ index.php
@@ -1,5 +1,41 @@
-<h1>Hola</h1>
 
 <?php
-echo "My first PHP script!";
+#Esto va imprimir:
+echo "<h1>My first PHP script!</h1><br>";
+print("Probando el print<br><br>");
+
+//Otra manera de comentar
+
+/*
+Varias
+Lineas
+de
+Comentarios
+*/
+
+#Variables
+$nombre = "Alan Lopez";
+$texto = "El autor de este script es $nombre";
+$altura = 175;
+$edad = $_GET["edad"];
+
+$textoFinal = "$texto, el cuenta con una estatura de ".$altura." cm <br>"; 
+
+echo $textoFinal;
+
+
+#Metodo GET
+echo "<hr>";
+echo "Mi edad es ".$edad."<br>";
+
+#Condiciones
+
+if($edad >= 18){
+    echo "Eres mayor de edad";
+}else{
+    echo "Eres menor de edad";
+}
+
+
+
 ?>
