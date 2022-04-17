1cac449df9e295a5ef03f64617584ff1b4359170
AlanJLJ2202 <lrs19110248@purisima.tecnm.mx>
2022-04-17 13:39:57 -0500

    Third

--- index.php
+++ index.php
@@ -17,7 +17,12 @@ Comentarios
 $nombre = "Alan Lopez";
 $texto = "El autor de este script es $nombre";
 $altura = 175;
-$edad = $_GET["edad"];
+
+
+#Comprobacion que se esta recibiendo el atributo edad desde el url
+#y si no, lo asigna directamente con variable estatica
+$edad = comprobarParametro("edad");
+
 
 $textoFinal = "$texto, el cuenta con una estatura de ".$altura." cm <br>"; 
 
@@ -31,11 +36,49 @@ echo "Mi edad es ".$edad."<br>";
 #Condiciones
 
 if($edad >= 18){
-    echo "Eres mayor de edad";
+    echo "Eres mayor de edad<br>";
 }else{
-    echo "Eres menor de edad";
+    echo "Eres menor de edad<br>";
+}
+
+#Funcion de variable especifica
+function comprobarEdad(){
+    if(isset($_GET["edad"])){
+        $edad = $_GET["edad"];
+    }else{
+        $edad = 21;
+    }
+    return $edad;
+}
+
+#Funcion con parametro indefinido
+function comprobarParametro($parametro){
+    if(isset($_GET[$parametro])){
+        $valor = $_GET[$parametro];
+    }else{
+        $valor = "Texto por defecto";
+    }
+    return $valor;
 }
 
 
+#Arrays
+$personas = ["Mauri", "Alan", "Gerson", "Toño"];
+
+echo "Imprimiendo el indice 2 de un array Personas: ";
+echo $personas[2]."<br>";
 
 ?>
+
+
+
+<h4>Listado</h4>
+<ul>
+    <?php
+        #Bucle
+        foreach($personas as $nombre){
+            echo "<li>$nombre</li>";
+        }
+
+    ?>
+</ul>
